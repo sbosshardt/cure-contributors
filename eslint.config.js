7d213b22437aed7c5dc81d5fc70986534ca79f14
Samuel Bosshardt <5762452+sbosshardt@users.noreply.github.com>
2024-11-27 15:50:19 -0800

    Improvements for linter. No more errors or warnings about Next.js plugin.

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,26 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
 import pluginReact from 'eslint-plugin-react'
+import pluginNext from '@next/eslint-plugin-next'
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
   {
     // Note: there should be no other properties in this object
-    ignores: ['.next/*/*'],
+    ignores: ['.next/*/*', 'out/*'],
   },
   {
     files: ['**/*.{js,mjs,cjs,jsx}'],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect the React version
+      },
+    },
+    plugins: {
+      '@next/next': pluginNext,
     },
     rules: {
       indent: ['error', 2], // Enforce 2-space indentation
--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 export default {
   output: 'standalone',
   distDir: 'out',
+  eslint: {
+    dirs: ['pages', 'components', 'src'], // Add your directories here
+  },
 }
